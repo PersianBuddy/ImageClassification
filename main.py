26c7b3938e160684c991dcf9347abe66aa2358ab
Shahin <mnajafi0014@gmail.com>
2024-04-27 20:52:42 +0330

    removing function that was unnassassary

--- main.py
+++ main.py
@@ -12,29 +12,6 @@ if os.path.exists(categorized_dir):
 # get source category
 source_dir = os.path.curdir
 
-
-
-# this function create basic direcotries for Zones
-def make_def_dir():
-    for i in range(1,16):
-        current_zone_dir = os.path.join(source_dir,f'Zone {i:02d}')
-        if not os.path.exists(current_zone_dir):
-            os.makedirs(current_zone_dir)
-        
-        branch_to_branch_dir = os.path.join(current_zone_dir,'Branch To Branch')
-        branch_to_fat_dir = os.path.join(current_zone_dir,'Branch To FAT')
-        fat_dir = os.path.join(current_zone_dir,'FAT')
-        # Check if the destination directory exists
-        if not os.path.exists(branch_to_branch_dir):
-            # If it doesn't exist, create the directory
-            os.makedirs(branch_to_branch_dir)
-        if not os.path.exists(branch_to_fat_dir):
-            # If it doesn't exist, create the directory
-            os.makedirs(branch_to_fat_dir)
-        if not os.path.exists(fat_dir):
-            # If it doesn't exist, create the directory
-            os.makedirs(fat_dir)
-
 # function that get all existing numbers in file name
 def get_number_in_text(text_with_number ):
     # Define a regular expression pattern to match the number after the text
